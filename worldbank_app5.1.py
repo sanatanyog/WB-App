f9aff9ca9a3b15d4eeec1f45f98491d6d99089f7
sanatanyog <yogsingh.py@gmail.com>
2025-06-25 13:40:26 +0530

    Update worldbank_app5.1.py

--- worldbank_app5.1.py
+++ worldbank_app5.1.py
@@ -13,7 +13,7 @@ INDICATORS = {
     '5': ('Inflation, consumer prices (annual %)', 'FP.CPI.TOTL.ZG'),
     '6': ('Population growth (annual %)', 'SP.POP.GROW'),
     '7': ('Central government debt, total (% of GDP)', 'GC.DOD.TOTL.GD.ZS'),
-    '8': ('Poverty Index ', 'POVERTY_AUTO'),
+    '8': ('Poverty Rate (%) ', 'POVERTY_AUTO'),
     '9': ('Unemployment Rate (%)', 'SL.UEM.TOTL.ZS'),
     '10': ('Corruption Control Score (WGI)', 'CC.EST')
 }
